7c42624d5531db2bf5e446dd94b0bbabb934b828
Steffan Poulsen <steffan@somatic.dk>
2020-05-22 13:00:13 +0200

    Release 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.2
+
+* Fixed a bug that caused the very first scene loaded, to hang indefinitely.
+
 # 1.0.1
 
 * Added compatibility for FoundryVTT 0.6.0
--- src/tokendragvisibility.js
+++ src/tokendragvisibility.js
@@ -5,33 +5,41 @@ var exceededThreshold = false;
 var inputDown = false;
 
 Hooks.on('ready', function() {
+    window.addEventListener("mousedown", checkDragBegin);
+    window.addEventListener('mousemove', checkDragMove);
+    window.addEventListener("mouseup", checkDragEnd);
+});
+
+function checkDragBegin(ev) {
     if (!game.user.isGM) return;
 
-    window.addEventListener("mousedown", function(ev) {
-        inputDown = true;
-        if (ev.button != 0 || focusToken == undefined || focusToken != hoverToken) return;
+    inputDown = true;
+    if (ev.button != 0 || focusToken == undefined || focusToken != hoverToken) return;
 
-        inputDelta = 0;
-        exceededThreshold = false;
-    });
+    inputDelta = 0;
+    exceededThreshold = false;
+}
 
-    window.addEventListener('mousemove', function(ev) {
-        if (exceededThreshold || focusToken == undefined || !inputDown) return;
+function checkDragMove() {
+    if (!game.user.isGM) return;
+
+    if (exceededThreshold || focusToken == undefined || !inputDown) return;
         
-        inputDelta += Math.abs(event.movementX) + Math.abs(event.movementY);
+    inputDelta += Math.abs(event.movementX) + Math.abs(event.movementY);
 
-        if (inputDelta > 32) {
-            canvas.sight.visible = false;
-            exceededThreshold = true;
-        }
-    });
+    if (inputDelta > 32) {
+        canvas.sight.visible = false;
+        exceededThreshold = true;
+    }
+}
 
-    window.addEventListener("mouseup", function() {
-        inputDown = false;
-        if (focusToken == undefined) return;
-        if (focusToken.data.vision && focusToken.owner) canvas.sight.visible = true;
-    });
-});
+function checkDragEnd() {
+    if (!game.user.isGM) return;
+
+    inputDown = false;
+    if (focusToken == undefined) return;
+    if (focusToken.data.vision && focusToken.owner) canvas.sight.visible = true;
+}
 
 Hooks.on("hoverToken", (token, isHovering) => {
     if (!game.user.isGM || inputDown) return;
@@ -40,7 +48,7 @@ Hooks.on("hoverToken", (token, isHovering) => {
 
 Hooks.on('controlToken', (token, hasControl) => {
     if (!game.user.isGM) return;
-    
+
     if (hasControl) {
         focusToken = token;
         hoverToken = token;
